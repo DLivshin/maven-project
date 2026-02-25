358d98015157edd82e9d788406eec69e1cc33ed0
David Livshin <davidli@jfrog.com>
2026-02-25 09:59:29 +0200

    initial reproduction setup

---
+++ src/main/java/test/testapplication/TestApplication.java
@@ -0,0 +1,7 @@
+package test.testapplication;
+
+public class TestApplication {
+    public static void main(String[] args) {
+        System.out.println("TestApplication Simulation running...");
+    }
+}
